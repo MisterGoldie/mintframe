0deef15d20557c08b9c7190726a882d835e006f7
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-08-22 10:02:06 -0500

    idk

--- api/index.tsx
+++ api/index.tsx
@@ -163,6 +163,7 @@ app.frame('/check', async (c) => {
     ),
     intents: [
       <Button action="/">Back</Button>,
+      <Button.Link href="https://polygonscan.com/token/0x3150e01c36ad3af80ba16c1836efcd967e96776e">Polygonscan</Button.Link>,
       <Button action="/check">Refresh Balance</Button>,
       fid === undefined ? <Button action="/connect">Connect Wallet</Button> : null
     ]
