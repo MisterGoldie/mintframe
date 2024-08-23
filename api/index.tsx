ba316a44fd8be407c1a2008f5e797eeee47cb005
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-08-23 02:28:29 -0500

    idk

--- api/index.tsx
+++ api/index.tsx
@@ -138,7 +138,7 @@ app.frame('/check', async (c) => {
   return c.res({
     image: (
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%', backgroundColor: '#FF8B19', padding: '20px', boxSizing: 'border-box' }}>
-        <h1 style={{ fontSize: '60px', marginBottom: '20px', textAlign: 'center' }}>Your $GOLDIES Balance on Polygon</h1>
+        <h1 style={{ fontSize: '60px', marginBottom: '20px', textAlign: 'center' }}>Your $GOLDIES balance</h1>
         <p style={{ fontSize: '48px', textAlign: 'center' }}>{fid !== undefined ? balanceDisplay : 'No connected Farcaster account found'}</p>
         <p style={{ fontSize: '36px', marginTop: '20px', textAlign: 'center' }}>Farcaster ID: {fid !== undefined ? fid : 'Not available'}</p>
         <p style={{ fontSize: '36px', marginTop: '10px', textAlign: 'center' }}>FID Source: {fidSource}</p>
